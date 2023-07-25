834c46040d1065ba245952c42d3d8e69f5ada669
Workchain Manipulator ⚡️ <95532896+craigwrong@users.noreply.github.com>
2023-07-25 18:36:57 +0100

    Update pageHome.swift

--- src/content/page/pageHome.swift
+++ src/content/page/pageHome.swift
@@ -4,7 +4,7 @@ let pageHome = Page("/", "Home") { #"""
 
 # Welcome, fellow Bitcoiner!
 
-This is a blog about Bitcoin and Bitcoin-only. I write primarily about Bitcoin development but also about The Lightning Network and other Bitcoin-centric technologies. I will never – ever – write about shitcoins so you can go ahead and bookmark this home page and stay at ease.
+This is a blog about Bitcoin and Bitcoin-only. I write primarily about Bitcoin development but also about The Lightning Network and other Bitcoin-centric technologies. I will never – ever – write about shitcoins so you can go ahead and bookmark this home page with confidence.
 
 Should you find anything of interest and would like to discuss just reach out to me on Twitter under handle [@notcraigwright](https://twitter.com/notcraigwright). Find out more about me [here](/about).
 
