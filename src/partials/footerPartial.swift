0247bdcd2a69e1cb0f815c46065c3025165ee7a9
Workchain Manipulator ⚡️ <95532896+craigwrong@users.noreply.github.com>
2023-07-25 19:07:44 +0100

    Update footerPartial.swift

--- src/partials/footerPartial.swift
+++ src/partials/footerPartial.swift
@@ -2,6 +2,6 @@ import SwiftySites
 
 let footerPartial = { (site: Site, page: Page?) -> String in """
 <footer>
-    <a href="https://github.com/craigwrong/craigwrong.github.io">Cypherpunks write code 👾</a>
+    <a href="https://github.com/swift-bitcoin/swift-bitcoin">Cypherpunks write code 👾</a>
 </footer>
 """ }
